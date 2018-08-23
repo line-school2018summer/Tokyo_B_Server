efa5d1b4e8f58af72517f70a96b7655f20e15382
delta114514 <32694778+delta114514@users.noreply.github.com>
2018-08-23 11:42:19 +0900

    Update umum.py

--- umum.py
+++ umum.py
@@ -141,12 +141,12 @@ def main():
                                   }))
 
 
-@app.route('/register', methods=['GET', 'POST'])  # アカウント登録用のディレクトリです
+@app.route('/account/register', methods=['GET', 'POST'])  # アカウント登録用のディレクトリです
 def register():
     if request.method == "GET":
         return make_response(jsonify({"error": 0,
                                       "content":
-                                          {"message": "/register[get]"}
+                                          {"message": "/account/register[get]"}
                                       }))
     exist_id = 0  # アカウントの存在
     authenticated = 0  # すでにログインしているか
@@ -155,6 +155,7 @@ def register():
     bad_password = 0  # passwordのよしあし
     password_confirm_does_not_match = 0  # passwordのコンファームが合致しているかどうかです・
     request_json = request.get_json()
+    print(request_json)
 
     if request_json["authenticated"]:
         authenticated = 1
@@ -200,12 +201,12 @@ def register():
     }))
 
 
-@app.route('/login', methods=["GET", "POST"])  # ログイン用のディレクトリです
+@app.route('/account/login', methods=["GET", "POST"])  # ログイン用のディレクトリです
 def login():
     if request.method == 'GET':
         return make_response(jsonify({"error": 0,
                                       "content":
-                                          {"message": "/login[get]"}
+                                          {"message": "/account/login[get]"}
                                       }))
 
     request_json = request.get_json()
@@ -240,12 +241,12 @@ def login():
         }))
 
 
-@app.route("/logout", methods=["GET", "POST"])
+@app.route("/account/logout", methods=["GET", "POST"])
 def logout():
     if request.method == "GET":
         return make_response(jsonify({"error": 0,
                                       "content": {
-                                          "message": "/logout[get]"
+                                          "message": "/account/logout[get]"
                                       }
                                       }))
     request_json = request.get_json()
@@ -272,12 +273,12 @@ def logout():
                                   }))
 
 
-@app.route('/account_modify', methods=['GET', 'POST'])  # ユーザー情報変更のディレクトリです。
+@app.route('/account/modify', methods=['GET', 'POST'])  # ユーザー情報変更のディレクトリです。
 def account_modify():
     if request.method == "GET":
         return make_response(jsonify({"error": 0,
                                       "content": {
-                                          "message": "/account_modify[get]"
+                                          "message": "/account/modify[get]"
                                       }
                                       }))
     not_authenticated = 0
@@ -334,12 +335,12 @@ def account_modify():
                                   }))
 
 
-@app.route("/add_friend", methods=["GET", "POST"])
+@app.route("/friend/add", methods=["GET", "POST"])
 def add_friend():
     if request.method == "GET":
         return make_response(jsonify({"error": 0,
                                       "content": {
-                                          "message": "/add_friend[get]"
+                                          "message": "/friend/add[get]"
                                       }
                                       }))
     request_json = request.get_json()
@@ -385,12 +386,12 @@ def add_friend():
                                   }))
 
 
-@app.route("/remove_friend", methods=["GET", "POST"])
+@app.route("/friend/remove", methods=["GET", "POST"])
 def remove_friend():
     if request.method == "GET":
         return make_response(jsonify({"error": 0,
                                       "content": {
-                                          "message": "/remove_friend[get]"
+                                          "message": "/friend/remove[get]"
                                       }
                                       }))
     request_json = request.get_json()
@@ -437,12 +438,12 @@ def remove_friend():
                                   }))
 
 
-@app.route("/friends_list", methods=["GET", "POST"])
+@app.route("/friend/list", methods=["GET", "POST"])
 def friends_list():
     if request.method == "GET":
         return make_response(jsonify({"error": 0,
                                       "content": {
-                                          "message": "/friends_list[get]"
+                                          "message": "/friend/list[get]"
                                       }
                                       }))
     request_json = request.get_json()
@@ -482,7 +483,6 @@ def chat_get():
                                       }))
     request_json = request.get_json()
     not_authenticated = 0
-    invalid_talk_id = 0
     invalid_verify = 0
 
     user = verify_token(request_json["id"], request_json["token"])
@@ -495,8 +495,7 @@ def chat_get():
         return make_response(jsonify({"error": 1,
                                       "content": {
                                           "not_authenticated": not_authenticated,
-                                          "invalid_verify": invalid_verify,
-                                          "invalid_talk_id": 0
+                                          "invalid_verify": invalid_verify
                                       }
                                       }))
     if request_json["content"]["talk_all_need"]:
@@ -544,13 +543,6 @@ def chat_get():
                                           }
                                       },
                                       }))
-    return make_response(jsonify({"error": 1,
-                                  "content": {
-                                      "not_authenticated": 0,
-                                      "invalid_verify": 0,
-                                      "invalid_talk_id": 1
-                                  }
-                                  }))
 
 
 @app.route("/chat/send", methods=["GET", "POST"])
@@ -582,7 +574,7 @@ def chat_send():
     talk = session.query(Talk_group).get(request_json["content"]["talk_id"])
     if not talk:
         invalid_talk_id = 1
-    if not_authenticated or invalid_verify or missing_target or too_long_text or meaningless_text or invalid_talk_id:
+    if not_authenticated or invalid_verify or too_long_text or meaningless_text or invalid_talk_id:
         return make_response(jsonify({"error": 1,
                                       "content": {
                                           "not_authenticated": not_authenticated,
@@ -606,12 +598,12 @@ def chat_send():
                                   }))
 
 
-@app.route("/chat/make/group", methods=["GET", "POST"])
+@app.route("/chat/make", methods=["GET", "POST"])
 def chat_goup_make():
     if request.method == "GET":
         return make_response(jsonify({"error": 0,
                                       "content": {
-                                          "message": "/chat/make/group[get]"
+                                          "message": "/chat/make[get]"
                                       }
                                       }))
 
@@ -777,256 +769,6 @@ def friend_search():
 
 # YYYY-MM-DD:HH:MM:SS
 
-
-"""
-
-@app.route("/json/date/<day>")
-def json_date(day):
-    try:
-        try:
-            global today
-            if today < datetime.date.today():
-                del_lim()
-        except:
-            del_lim()
-            today = datetime.date.today()
-        jsons = {i: "" for i in day.replace(" ", "").split(";")}
-        for date in day.split(";"):
-            tmp = date.split("-")
-            tmp = datetime.date(*map(int, tmp))
-            p_from = Entry.query.filter(Entry.change_from_date.in_([date])).order_by(Entry.target_depart).all()
-            p_to = Entry.query.filter(Entry.change_to_date.in_([date])).order_by(Entry.target_depart).all()
-            p_all = list(set(p_from) | set(p_to))
-            json = {
-                "from_match": {str(i): json_it(m) for i, m in enumerate(p_from)
-                               },
-                "to_match": {str(i): json_it(m) for i, m in enumerate(p_to)
-                             },
-                "all_match": {str(i): json_it(m) for i, m in enumerate(p_all)
-                              }
-            }
-            json["from_match"]["count"] = len(p_from)
-            json["to_match"]["count"] = len(p_to)
-            json["all_match"]["count"] = len(p_all)
-            jsons[date] = json
-        return jsonify(jsons)
-    except:
-        abort(500)
-
-
-@app.route("/json/class/<depart>")
-def json_depart(depart):
-    try:
-        global dat
-        try:
-            global today
-            if today < datetime.date.today():
-                del_lim()
-        except:
-            del_lim()
-            today = datetime.date.today()
-        p = Entry.query.all()
-
-        dat_rev = {value: key for key, value in dat.items()}
-
-        tmp = set(dat_rev[i] for i in depart.upper().replace(" ", "").split(";"))
-        over = defaultdict(list)
-        for i in p[::]:
-            if not prime_factors(i.target_depart) & tmp:
-                p.remove(i)
-            for j in tmp:
-                if j in prime_factors(i.target_depart):
-                    over[dat[j]] += [i]
-        json = {
-            "all_match": {str(i): json_it(m) for i, m in enumerate(p)
-                          }
-        }
-        json["all_match"]["count"] = len(p)
-        for key, value in over.items():
-            json[key] = {str(k): json_it(m) for k, m in enumerate(value)}
-            json[key]["count"] = len(value)
-        return jsonify(json)
-    except:
-        abort(500)
-
-
-@app.route("/json/reference")
-def api_reference():
-    return render_template("reference.html")
-
-
-@app.route("/edit/<int:num>", methods=['GET', 'POST'])
-@login_required
-def edit(num=0):
-    p = Entry.query.filter(Entry.changeid.in_([num])).first()
-    if not p:
-        return render_template("404.html")
-    global dat
-    if request.method == 'POST':
-        edited = 0
-        tmp = 1
-        for key, item in request.form.items():
-            if key == "all":
-                edited = 1
-                break
-            if key[:5] == "radio":
-                tmp *= int(item)
-        if tmp == 1 and not edited:
-            tmp = p.target_depart
-        if p.contributor != current_user.id:
-            return render_template("404.html")
-        p.target_depart = tmp
-        p.change_from_date = request.form["from_date"]
-        p.change_from_class = request.form["from_class"]
-        p.change_from_time = request.form["from_time"]
-        p.change_from_teacher = request.form["from_teacher"]
-        p.change_to_date = request.form["to_date"]
-        if request.form["from_class"] == request.form["to_class"]:
-            p.change_to_class = ""
-        else:
-            p.change_to_class = request.form["to_class"]
-        p.change_to_time = request.form["to_time"]
-        p.change_to_teacher = request.form["to_teacher"]
-        p.remark = request.form["remark"]
-        p.published = request.form["published"]
-        if str(request.form["delete"]) == "いいよ！こいよ！":
-            session.delete(p)
-            session.commit()
-            return redirect("/")
-        session.commit()
-    return render_template('editprofile.html', page=p, prim=prime_factors, dat=dat)
-
-
-@app.route("/<passw>/count")
-def count(passw):
-    global hashed_pass
-    if hashlib.sha256(b"%a" % passw).digest() == hashed_pass:
-        return render_template("count.html", que=View.query.all())
-    else:
-        return redirect("404.html")
-
-
-@app.route("/proposal", methods=["GET", "POST"])
-def proposal():
-    if request.method == "GET":
-        return render_template("proposal.html")
-    else:
-        with open("art.txt", "a") as file:
-            file.write(request.form["string"].replace("[EOA]", "") + "[EOA]")
-        flash("メッセージが投稿されました。")
-        return render_template("proposal.html")
-
-
-@app.route("/proposals/<passw>")
-def proposals(passw):
-    if hashlib.sha256(
-                    b"%a" % passw).digest() == b'\xf8\x83\xa7i;\x17 \xe3\xf4\x7f\xb8j\xbe\xd6I\xd2*\xeb\xe9*:c\xcf\xc9\x0e\xb5\xe3Lu\xb4\x94\xd9':
-        try:
-            with open("art.txt", "r") as file:
-                tmp = file.read().split("[EOA]")[:-1]
-            txt = "<table border=\"1\" width=\"400\"><tr><th>body</th></tr><tr><td>" + "</td></tr><tr><td>".join(
-                tmp) + "</tb></tr></table>"
-            return txt
-        except:
-            return "no proposals"
-
-
-@app.route("/flush")
-@login_required
-def flush():
-    del_lim()
-    return "flushed!"
-
-
-@app.route('/<passw>/static/<filename>')
-def static_dir(passw, filename):
-    global hashed_pass
-    if hashlib.sha256(b"%a" % passw).digest() == hashed_pass:
-        return send_from_directory('static', filename)
-    else:
-        return redirect("404.html")
-
-
-@app.route('/<passw>/image/<filename>')
-def image_dir(passw, filename):
-    global hashed_pass
-    if hashlib.sha256(b"%a" % passw).digest() == hashed_pass:
-        return send_from_directory('image', filename)
-    else:
-        return redirect("404.html")
-
-
-@app.route('/file/<filename>')
-@login_required
-def files(filename):
-    return send_from_directory("./", filename)
-
-
-def feed_cookie(content, cookie, now):
-    response = make_response(content)
-    max_age = 60 * 60 * 24 * 120
-    expires = int(datetime.datetime.now().timestamp()) + max_age
-    response.set_cookie('depart', value=str(cookie * 810893), max_age=max_age)
-    return response
-
-
-def intize(cookie):
-    return int(cookie.get("depart"))
-
-
-def json_it(entry):
-    dat = {2: '1M', 3: '1E', 5: '1C', 7: '1A', 11: '2M', 13: '2E', 17: '2C', 19: '2A', 23: '3M', 29: '3E',
-           31: '3C', 37: '3A', 41: '4M', 43: '4EJ', 47: '4ED', 53: '4C', 59: '4A', 61: '5M', 67: '5EJ',
-           71: '5ED', 73: '5C', 79: '5A', 83: '1A.ME', 89: '1A.CA', 97: '2A.ME', 101: '2A.CA'}
-    json = {
-        "from": {
-            "class": entry.change_from_class,
-            "date": entry.change_from_date,
-            "time": entry.change_from_time,
-            "teacher": entry.change_from_teacher
-        },
-        "to": {
-            "class": entry.change_to_class,
-            "date": entry.change_to_date,
-            "time": entry.change_to_time,
-            "teacher": entry.change_to_teacher
-        },
-        "depart": ";".join([dat[i] for i in prime_factors(entry.target_depart)]),
-        "remark": entry.remark
-    }
-    return json
-
-
-def del_lim():
-    p = Entry.query.all()
-    deleted = False
-    for ent in p:
-        try:
-            bef = datetime.date(*list(map(int, ent.change_from_date.split("-"))))
-        except:
-            bef = datetime.date(1, 1, 1)
-        try:
-            to = datetime.date(*list(map(int, ent.change_to_date.split("-"))))
-        except:
-            to = datetime.date(1, 1, 1)
-        if max(bef, to) < datetime.date.today():
-            session.delete(ent)
-            deleted = True
-    if deleted:
-        session.commit()
-
-
-def prime_factors(n):
-    global all_set
-    if not n:
-        return set()
-    i = 2
-    factors = []
-    for i in all_set:
-        if not n % i:
-            factors.append(i)
-    return set(factors)
-"""
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
