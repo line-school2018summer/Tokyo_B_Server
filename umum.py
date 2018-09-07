db5355eb444e7b1719acbe4e090df63ec4ca818a
yuma_suzuki <s.yuma.2990.mawasi@gmail.com>
2018-09-07 16:38:26 +0900

    [modify] fixbugs

--- umum.py
+++ umum.py
@@ -486,7 +486,8 @@ def add_friend():
     if request_json["use_id"]:
         target = session.query(User).get(request_json["target_id"])
     else:
-        target = session.query(User).filter(User.uesr_id.in_(request_json["target_id"])).first()
+        print([request_json["target_id"]])
+        target = session.query(User).filter(User.user_id.in_([request_json["target_id"]])).first()
     if not target:
         unexist_id = 1
     if user == target:
@@ -971,6 +972,7 @@ def friend_search():
     if request_json["content"]["use_id"]:
         target_user = session.query(User).get(request_json["content"]["target_user_id"])
     else:
+        print([request_json["content"]["target_user_id"]])
         target_user = session.query(User).filter(
             User.user_id.in_([request_json["content"]["target_user_id"]])).first()
 
